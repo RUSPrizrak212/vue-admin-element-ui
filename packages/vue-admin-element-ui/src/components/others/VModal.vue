1c25945f7ba987c63be0216fc2938f1cd0a116e8
i.ermakov <i.ermakov@asol.pro>
2024-08-02 16:52:01 +0300

    update VModal.vue

--- packages/vue-admin-element-ui/src/components/others/VModal.vue
+++ packages/vue-admin-element-ui/src/components/others/VModal.vue
@@ -1,30 +1,37 @@
 <template>
-    <div
-        class="fixed inset-0 z-30 bg-gray-200 p-10 bg-opacity-30 flex justify-center items-start transition overflow-y-scroll"
-    >
-        <div :class="modalClasses" class="bg-white w-full flex flex-col items-center shadow-md rounded-md">
-            <div
-                class="px-4 py-2 flex justify-between items-center rounded-t-md font-light text-gray-100 bg-gray-600 text-l w-full"
-            >
-                <span>{{ title }}</span>
-                <span class="cursor-pointer group" @click="$emit('close')">
-                    <cross class="w-4 text-gray-100 hover:text-gray-50" />
-                </span>
-            </div>
-            <div :class="bodyClass" class="pt-4 pb-6 px-8 w-full h-full">
-                <slot />
+    <teleport to="body">
+        <div
+            class="fixed inset-0 z-30 bg-gray-200 p-10 bg-opacity-30 flex justify-center items-start transition overflow-y-scroll"
+        >
+            <div :class="modalClasses" class="bg-white w-full flex flex-col items-center shadow-md rounded-md">
+                <div
+                    class="px-4 py-2 flex justify-between items-center rounded-t-md font-light text-gray-100 bg-gray-600 text-l w-full"
+                >
+                    <span>{{ title }}</span>
+                    <span class="cursor-pointer group" @click="$emit('close')">
+                        <cross class="w-4 text-gray-100 hover:text-gray-50" />
+                    </span>
+                </div>
+                <div :class="bodyClass" class="pt-4 pb-6 px-8 w-full h-full">
+                    <slot />
 
-                <div v-if="confirmButtons" class="flex space-x-2 pt-7" :class="positionClass">
-                    <v-button color="white" size="sm" @click="$emit('close')">
-                        {{ $vueAdmin.t('CANCEL') }}
-                    </v-button>
-                    <v-button :loading="loading" size="sm" @click="$emit('confirm')" :disabled="confirmButtonDisabled">
-                        {{ localConfirmButtonText }}
-                    </v-button>
+                    <div v-if="confirmButtons" class="flex space-x-2 pt-7" :class="positionClass">
+                        <v-button color="white" size="sm" @click="$emit('close')">
+                            {{ $vueAdmin.t('CANCEL') }}
+                        </v-button>
+                        <v-button
+                            :loading="loading"
+                            size="sm"
+                            @click="$emit('confirm')"
+                            :disabled="confirmButtonDisabled"
+                        >
+                            {{ localConfirmButtonText }}
+                        </v-button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </teleport>
 </template>
 
 <script lang="ts">
